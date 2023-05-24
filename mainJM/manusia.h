11b10f6cb1134d3389b9a8e416bddb3555968b82
andiharun29 <andiharunalrasyid14@gmail.com>
2023-05-24 18:20:52 +0700

    membuat member dari calass manusia

--- mainJM/manusia.h
+++ mainJM/manusia.h
@@ -3,6 +3,18 @@
 using namespace std;
 
 class manusia {
+public:
+	string name;
+	jantung varjantung;
+
+	manusia(string pname)
+		: name(pnmae) {
+		cout << name << " hidup\n";
+	}
+	~manusia() {
+		cout << name << "mati\n";
+	}
+};
 
 #endif // !MANUSIA_H
 
