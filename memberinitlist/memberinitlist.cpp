6906de678fa52af14a6b28deb5d0ddc705180688
andiharun29 <andiharunalrasyid14@gmail.com>
2023-05-24 18:31:30 +0700

    membuat member public

--- memberinitlist/memberinitlist.cpp
+++ memberinitlist/memberinitlist.cpp
@@ -8,4 +8,13 @@ private:
 	string nama;
 	float nilai;
 
+public:
+	mahasiswa(int pid, string pnama, float pnilai) :id(pid), nama(pnama), nilai(pnilai) {
+
+	}
+	~mahasiswa() {
+		cout << "id    =" << id << endl;
+		cout << "nama  =" << nama << endl;
+		cout << "nilai =" << nilai << endl;
+	}
 };
